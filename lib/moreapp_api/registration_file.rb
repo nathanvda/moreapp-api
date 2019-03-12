0fd433c96c8ae5a2eb47bd74ec4a1ec87d55efbc
nathanvda <nathan@dixis.com>
2019-03-12 16:56:26 +0100

    Add api to upload an image for use in an instruction

---
+++ lib/moreapp_api/registration_file.rb
@@ -0,0 +1,29 @@
+class MoreappAPI
+  class RegistrationFile
+
+
+    def initialize(customer)
+      @customer = customer
+    end
+
+    def post(filename, content_type = 'image/png')
+      # !!! Undocumented more-app API
+
+      image_data = Base64.encode64(File.open(filename, "rb").read)
+
+      response = @customer.moreapp_api.request(:post, "/api/v1.0/client/registrations/files/base64", nil, {},
+                                  {
+                                      customerNumber: customer.id,
+                                      base64FileContent: "data:#{content_type};base64," + image_data,
+                                      filename: File.basename(filename)
+                                  }.to_json,
+                                  { 'Content-Type' => 'application/json' } )
+
+      # returns a body containing the location of the image. Looks something like "gridfs://registrationFiles/#{image_location_id}"
+      # which can then be used as a value for a "photo" - field
+      response.body
+    end
+
+
+  end
+end
